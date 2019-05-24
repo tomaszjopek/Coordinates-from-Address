fabe67bb58f8aa73d0e80a2719ba40e081d62550
Tomasz <tom.jopek@gmail.com>
2019-05-24 10:05:48 +0200

    init commit

---
+++ src/main/java/com/pwr/jopek/geo_data/AppConfig.java
@@ -0,0 +1,11 @@
+package com.pwr.jopek.geo_data;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AppConfig {
+
+
+
+}
---
+++ src/main/java/com/pwr/jopek/geo_data/GeoDataApplication.java
@@ -0,0 +1,13 @@
+package com.pwr.jopek.geo_data;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeoDataApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GeoDataApplication.class, args);
+    }
+
+}
